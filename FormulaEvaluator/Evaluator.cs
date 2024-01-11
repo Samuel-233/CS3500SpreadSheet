ebb36e47e93be39b014fd062ae883665c8aa57a7
Sam <2122783374@qq.com>
2024-01-11 13:25:39 -0700

    Add a header and readme

--- FormulaEvaluator/Evaluator.cs
+++ FormulaEvaluator/Evaluator.cs
@@ -1,6 +1,23 @@
 ﻿namespace FormulaEvaluator
 {
-//testing 
+    /// <summary>
+    /// Author:    Shu Chen
+    /// Partner:   [Partner Name or None]
+    /// Date:      2024/1/10
+    /// Course:    CS 3500, University of Utah, School of Computing
+    /// Copyright: CS 3500 and [Your Name(s)] - This work may not 
+    ///            be copied for use in Academic Coursework.
+    ///
+    /// I, Shu Chen, certify that I wrote this code from scratch and
+    /// did not copy it in part or whole from another source.  All 
+    /// references used in the completion of the assignments are cited 
+    /// in my README file.
+    ///
+    /// File Contents
+    ///
+    ///    [... and of course you should describe the contents of the 
+    ///    file in broad terms here ...]
+    /// </summary>
     public class Evaluator
     {
 
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Spreadsheet
+```
+Author:     Shu Chen
+Partner:    None
+Start Date: 10-Jan-2024
+Course:     CS 3500, University of Utah, School of Computing
+GitHub ID:  Samuel-233
+Repo:       https://github.com/uofu-cs3500-spring24/spreadsheet-Samuel-233
+Commit Date:Day-Month-Year Time (of when submission is ready to be evaluated)
+Solution:   Spreadsheet
+Copyright:  CS 3500 and Shu Chen - This work may not be copied for use in Academic Coursework.
+```
+
+# Overview of the Spreadsheet functionality
+
+The Spreadsheet program is currently capable of ....  Future extensions are ...
+
+# Time Expenditures:
+
+    1. Assignment One:   Predicted Hours:          10        Actual Hours:   20
+    2. Assignment Two:   Predicted Hours:           8        Actual Hours:    4
